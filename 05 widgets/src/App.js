e1a1d59686036ff63949a3a40b8a38e64b322b9e
AsterinGray <raymondtiandinata@gmail.com>
2021-05-18 17:58:09 +0700

    Search Widget Done

--- 05 widgets/src/App.js
+++ 05 widgets/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Accordion from './components/Accordion';
+import Search from './components/Search';
 
 const items = [
   {
@@ -19,7 +20,8 @@ const items = [
 const App = () => {
   return (
     <h1>
-      <Accordion items={items} />
+      {/* <Accordion items={items} /> */}
+      <Search />
     </h1>
   );
 };
